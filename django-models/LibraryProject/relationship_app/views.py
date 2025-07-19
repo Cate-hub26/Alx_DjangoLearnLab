1bebe259a5475a13e2139b590e1d319901673f8c
Cate <catherinenzisa2017@gmail.com>
2025-07-19 20:45:34 +0300

    Update view.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,8 +7,9 @@ from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
 
 def list_books(request):
     books = Book.objects.all()
