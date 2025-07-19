11d8f4213e9ba57ebb755200d8f7ac7e0f6025dd
Cate <catherinenzisa2017@gmail.com>
2025-07-19 18:40:30 +0300

    Update the contents of relationship_app directory.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,8 +3,8 @@ from django.views.generic.detail import DetailView
 from .models import Book, Library
 
 def list_books(request):
-    books = Book.objects.select_related('author').all()
-    return render(request, 'books/book_list.html', {'books': books})
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
