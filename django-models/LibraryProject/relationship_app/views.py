8b34d5b043664389baa770b4707aa002dd1c2784
Cate <catherinenzisa2017@gmail.com>
2025-07-20 16:38:31 +0300

    Modify urls.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -56,6 +56,14 @@ def librarian_view(request):
 @permission_required('relationship_app.can_add_book')
 def add_book(request):
     return render(request, 'relationship_app/add_book.html')
+
+@permission_required('relationship_app.can_change_book')
+def edit_book(request):
+    return render(request, 'relationship_app/change_book.html')
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request):
+    return render(request, 'relationship_app/delete_book.html')
     
 
         
