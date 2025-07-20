d44e7526ac75dcd7738851ec2af2357603a2a773
Cate <catherinenzisa2017@gmail.com>
2025-07-20 14:01:08 +0300

    Update views.py file

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from django.contrib.auth import User
+from django.contrib.auth.decorators import user_passes_test, permission_required
 
 class Author(models.Model):
     name = models.CharField(max_length=200)
     
     def __str__(self):
         return self.name
-        
-class Book(models.Model):
+    
+@permission_required        
+class Book(models.Model):    
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    class Meta():
+        permissions = ('can_add_book', 'can_change_book', 'can_delete_book')
     
     def __str__(self):
         return self.title
@@ -26,3 +31,8 @@ class Librarian(models.Model):
     
     def __str__(self):
         return self.name
+    
+@user_passes_test
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='userprofile')
+    role = models.CharField(max_length=200, choices=('Admin', 'Librarian', 'Member'))
