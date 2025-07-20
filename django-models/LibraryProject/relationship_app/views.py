6f29862c0987862d5eab2f0ffbaa3f17187f017c
Cate <catherinenzisa2017@gmail.com>
2025-07-20 16:30:09 +0300

    Modify views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView 
 from django.urls import reverse_lazy
-from django.contrib.auth.decorators import  permission_required, user_passes_test, login_required
+from django.contrib.auth.decorators import permission_required, user_passes_test, login_required
 
 class SignUpView(CreateView):
     form_class = UserCreationForm()
