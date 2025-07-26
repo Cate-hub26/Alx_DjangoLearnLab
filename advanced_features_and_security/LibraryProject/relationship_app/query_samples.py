76ca7b9c6504fe7c8afde814abf25aa042eade26
Cate <catherinenzisa2017@gmail.com>
2025-07-26 12:38:09 +0300

    Add advanced_features_and_security folder

---
+++ advanced_features_and_security/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,19 @@
+from .models import Book, Library, Librarian, Author
+
+#Query all books by a specific author
+def books_by_specific_author(author_name):
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
+
+
+#Retrieve the librarian for a library
+def librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
+
+
+#List all books in a library
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
