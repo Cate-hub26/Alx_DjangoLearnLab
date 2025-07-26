a96256e7c91e3d7ccc0cf5a67388184a9b9bdfa0
Cate <catherinenzisa2017@gmail.com>
2025-07-26 15:47:58 +0300

    Update models.py and views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -14,9 +14,9 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
 
-def list_books(request):
+def book_list(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, 'relationship_app/book_list.html', {'books': books})
 
 
 from .models import Library
