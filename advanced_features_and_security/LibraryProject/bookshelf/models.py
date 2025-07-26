a3b5a2a0a1b9a3725e254ab889da6eadb5eed3d1
Cate <catherinenzisa2017@gmail.com>
2025-07-26 12:42:57 +0300

    Add advanced_features_and_security folder

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,72 @@
 from django.db import models
+from django.contrib.auth import User
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.decorators import permission_required
+
+from django.db import models
+from django.contrib.auth import User
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.decorators import permission_required
+
+class CustomUser(models.AbstractUser):
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
+    
+    def create_user(model, fields):
+        model = CustomUser
+        fields = ['date_of_birth', 'profile_photo']
+        
+    def create_superuser(admin):
+        admin = CustomUser()
+        
+class ModelAdmin(CustomUser):
+    
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+    
+@permission_required        
+class Book(models.Model):    
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    
+    class Meta:
+        permissions = (
+            ('can_add_book', 'Can add a book'),
+            ('can_change_book', 'Can change a book'),
+            ('can_delete_book', 'Can delete a book'),
+        )
+    
+    def __str__(self):
+        return self.title
+    
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book, related_name='libraries')
+    
+    def __str__(self):
+        return self.name
+    
+class Librarian(models.Model):
+    name = models.CharField(max_length=200)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+class UserProfile(models.Model):
+    Role_Choices = (
+    ('Admin', 'Administrator'),
+    ('Librarian', 'Library Staff'),
+    ('Member', 'Registered Member'),
+    )
+    user = models.OneToOneField(CustomUser, related_name='userprofile')
+    role = models.CharField(max_length=200, choices=Role_Choices)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
