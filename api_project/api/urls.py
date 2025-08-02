9e5ea79636bdfd592bdde2d28f86edacdfdb8757
Cate <catherinenzisa2017@gmail.com>
2025-08-02 13:06:42 +0300

    Update api/urls.py file to contain BookList.as_view().

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from .views import BookListCreateAPIView, BookViewSet
+from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='books_all')
 
 urlpatterns = [
-    path('books/', BookListCreateAPIView.as_view(), name='book-list'),
+    path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,7 +8,7 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookListCreateAPIView(generics.ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
