818cafaaae4f26a1aaf7d651212220d51e9d8992
Cate <catherinenzisa2017@gmail.com>
2025-08-02 13:56:33 +0300

    Update settings.py, views.py and url.py files to include authetication

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='books_all')
@@ -8,4 +10,5 @@ router.register(r'books_all', BookViewSet, basename='books_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
+    path('token/', obtain_auth_token, name='token_auth'),
 ]
