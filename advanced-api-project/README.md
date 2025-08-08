7ce169411b8126ac9558da745c90f22e12360983
Cate <catherinenzisa2017@gmail.com>
2025-08-08 17:28:51 +0300

    Update the views.py and READM.md files to meet the requirements of the ALX Checker.

--- advanced-api-project/README.md
+++ advanced-api-project/README.md
@@ -1,9 +1,9 @@
 
 # Book API Views Overview
 
-## BookListCreateView
+## BookListView
 
-**Class**: `ListCreateAPIView`  
+**Class**: `ListAPIView`  
 **Model**: `Book`  
 **Serializer**: `BookSerializer`  
 **Permissions**: `IsAuthenticatedOrReadOnly`
@@ -11,15 +11,27 @@
 ### Purpose
 Provides an endpoint to:
 - List all books (`GET`)
-- Create a new book (`POST`)
 
 ### Behavior
 - **GET**: Accessible to all users. Returns a list of all book records.
+
+## BookCreateView
+
+**Class**: `CreateAPIView`  
+**Model**: `Book`  
+**Serializer**: `BookSerializer`  
+**Permissions**: `IsAuthenticatedOrReadOnly`
+
+### Purpose
+Provides an endpoint to:
+- Create a new book (`POST`)
+
+### Behavior
 - **POST**: Restricted to authenticated users. Allows creation of a new book.
 
-## BookDetailUpdateDeleteView
+## BookDetailView
 
-**Class**: `RetrieveUpdateDestroyAPIView`  
+**Class**: `RetrieveAPIView`  
 **Model**: `Book`  
 **Serializer**: `BookSerializer`  
 **Permissions**: `IsAuthenticatedOrReadOnly`
@@ -27,9 +39,34 @@ Provides an endpoint to:
 ### Purpose
 Provides an endpoint to:
 - Retrieve a single book (`GET`)
-- Update a book (`PUT`/`PATCH`)
-- Delete a book (`DELETE`)
 
 ### Behavior
 - **GET**: Accessible to all users. Returns details of a specific book.
-- **PUT/PATCH/DELETE**: Restricted to authenticated users. Allows modification or deletion of a book.
+
+## BookUpdateView
+
+**Class**: `UpdateAPIView`  
+**Model**: `Book`  
+**Serializer**: `BookSerializer`  
+**Permissions**: `IsAuthenticatedOrReadOnly`
+
+### Purpose
+Provides an endpoint to:
+- Update a book (`PUT`/`PATCH`)
+
+### Behavior
+- **PUT/PATCH**: Restricted to authenticated users. Allows modification or deletion of a book.
+
+## BookDeleteView
+
+**Class**: `DestroyAPIView`  
+**Model**: `Book`  
+**Serializer**: `BookSerializer`  
+**Permissions**: `IsAuthenticatedOrReadOnly`
+
+### Purpose
+Provides an endpoint to:
+- Delete a book (`DELETE`)
+
+### Behavior
+- **DELETE**: Restricted to authenticated users. Allows modification or deletion of a book.
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,20 +4,42 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
 
-# This view handles listingall books(GET) and creating a new book(POST).
+# This view handles listing all books(GET).
 # GET: Allows all users to access the books.
-# POST: Restricted to authenticated users only.
 # Uses IsAuthenticatedOrReadOnly to enforce permissions.
-class BookListCreateView(generics.ListCreateAPIView):
+class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     
-# This view handles listing a single book(GET), updating(PUT/PATCH) and deleting(DELETE).
+# This view handles creating a new book(POST).
+# POST: Restricted to authenticated users only.
+# Uses IsAuthenticatedOrReadOnly to enforce permissions
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+# This view handles listing a single book(GET).
 # GET: Accessible to all users
-# PUT/PATCH/DELETE: Accessible to authenticated users only
 # Uses IsAuthenticatedOrReadOnly to enforce permissions.
-class BookDetailUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+# This view handles updating(PUT/PATCH).
+# PUT/PATCH: Accessible to authenticated users only
+# Uses IsAuthenticatedOrReadOnly to enforce permissions.
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+# This view handles deleting(DELETE).
+# DELETE: Accessible to authenticated users only
+# Uses IsAuthenticatedOrReadOnly to enforce permissions.
+class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
