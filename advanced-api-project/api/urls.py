5ee870b52049bb260d142e28a05ebffec41fde0e
Cate <catherinenzisa2017@gmail.com>
2025-08-08 17:40:31 +0300

    Update the urls.py file to include books/create, books/update, books/delete .

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import BookListCreateView, BookDetailUpdateDeleteView
+from .views import (BookListView, BookCreateView, 
+BookDetailView, BookUpdateView, BookDeleteView)
 
 urlpatterns = [
-    path('/books/', BookListCreateView.as_view(), name='book-list-create'),
-    path('/books/<int:pk>/', BookDetailUpdateDeleteView.as_view(), name='book-detail-update-delete'),
+    path('books/list', BookListView.as_view(), name='books-list'),
+    path('books/create/<int:pk>/', BookCreateView.as_view(), name='books-create'),
+    path('books/detail/<int:pk>/', BookDetailView.as_view(), name='books-detail'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='books-update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='books-delete'),
 ]
