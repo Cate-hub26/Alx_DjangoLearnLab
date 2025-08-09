79ca9f53fdccc94eabba3b5ced22bfd8ead699d5
Cate <catherinenzisa2017@gmail.com>
2025-08-09 13:58:49 +0300

    Add test_views.py file.

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,9 +3,9 @@ from .views import (BookListView, BookCreateView,
 BookDetailView, BookUpdateView, BookDeleteView)
 
 urlpatterns = [
-    path('books/list', BookListView.as_view(), name='books-list'),
-    path('books/create/<int:pk>/', BookCreateView.as_view(), name='books-create'),
-    path('books/detail/<int:pk>/', BookDetailView.as_view(), name='books-detail'),
-    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='books-update'),
-    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='books-delete'),
+    path('books/list', BookListView.as_view(), name='books_list'),
+    path('books/create', BookCreateView.as_view(), name='books_create'),
+    path('books/detail/<int:pk>/', BookDetailView.as_view(), name='books_detail'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='books_update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='books_delete'),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,4 @@
-from django_filters import rest_framework 
+#from django_filters import rest_framework 
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django.shortcuts import render
 from rest_framework import generics, filters, fields
