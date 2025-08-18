2b8c645e5786b3facf7d63bd0e036c9cbeeac478
Cate <catherinenzisa2017@gmail.com>
2025-08-18 18:25:19 +0300

    Update serializers.py to meet the requirements of the ALX Checker.

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,14 +1,19 @@
 from rest_framework import serializers
-from rest_framework.serializers import ModelSerializer
-from.models import CustomUser
+from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
 
 class RegisterUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
-        extra_kwargs = {'password': {'write_only': True}}
-        
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+
     def create(self, validated_data):
-        validated_data['password'] = make_password(validated_data['password'])
-        return super().create(validated_data)
+        password = validated_data.pop('password')
+        user = User.objects.create_user(password=password, **validated_data)
+        Token.objects.create(user=user)
+        return user
