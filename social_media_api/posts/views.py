48883ade5a1b821b7108b68b131008c4e57158f7
Cate <catherinenzisa2017@gmail.com>
2025-08-20 15:17:04 +0300

    Update social_media_api/posts/views to meet the requirements of the ALX Checker.

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, filters
 from rest_framework.views import APIView
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
@@ -11,7 +11,7 @@ from rest_framework.response import Response
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
     filter_backends = [filters.SearchFilter]
     search_fields = ['title', 'content']
     
@@ -21,24 +21,24 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['post']
     
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-        
+
 class FeedView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
     
     def get(self, request):
         # Get users the current user is following
         
-        followed_users = request.user.following.all()
+        following_users = request.user.following.all()
         
         # Get posts from those users, ordered by newest first
         
-        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
         
         # Serialize and return the data
         serializer = PostSerializer(posts, many=True)
